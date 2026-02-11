d0702f1820af92548d12d3202c92386ccd2bb050
Xusayn1 <Xusayn@gmail.com>
2026-02-11 09:40:04 +0500

    improved css styles and home page was added

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def about(request):
     return render(request, 'main/index.html')  
+
+
+def home(request):
+    return render(request, 'main/index2.html')
